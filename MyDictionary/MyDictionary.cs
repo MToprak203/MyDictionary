fc3a129123d2651bcd1c2548a7df6d8a7d2dcfe4
Mustafa Toprak <MToprak.L203@gmail.com>
2021-01-23 17:28:39 +0300

    MyDictionary

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,48 @@
+﻿namespace MyDictionary
+{
+    class MyDictionary<T,K>
+    {
+        T[] Keys;
+        K[] Values;
+
+        public MyDictionary()
+        {
+            Keys = new T[0];
+            Values = new K[0];
+        }
+
+        public void Add(T key, K value)
+        {
+            T[] tempKeys = Keys;
+            K[] tempValues = Values;
+
+            Keys = new T[Keys.Length + 1];
+            Values = new K[Values.Length + 1];
+
+            for(int i = 0; i < tempKeys.Length; i++)
+            {
+                Keys[i] = tempKeys[i];
+                Values[i] = tempValues[i];
+            }
+
+            Keys[Keys.Length - 1] = key;
+            Values[Values.Length - 1] = value;
+        }
+
+       /* public K GetValue(T key)
+        {
+            int i;
+            for (i = 0; i < Keys.Length; i++)
+            {
+                if (Keys[i].Equals(key))
+                    break;
+            }
+
+            if (i == Keys.Length)
+                return default(K);
+
+            return Values[i];
+        }
+       */
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> dict = new MyDictionary<string, int>();
+
+            dict.Add("a", 1);
+            dict.Add("b", 2);
+            dict.Add("c", 4);
+            dict.Add("d", 8);
+
+            /*
+            Console.WriteLine(dict.GetValue("a"));
+            Console.WriteLine(dict.GetValue("b"));
+            Console.WriteLine(dict.GetValue("c"));
+            Console.WriteLine(dict.GetValue("d"));
+            */
+        }
+    }
+}
